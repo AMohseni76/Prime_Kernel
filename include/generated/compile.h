0722d63d9e643179f4b1e3fb4389546c7e0ab3f7
Alireza Mohseni <gascan1376@gmail.com>
2016-09-06 14:32:55 +0430

    9

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Wed Aug 31 20:02:05 IRDT 2016"
+#define LINUX_COMPILE_BY "amohseni76"
+#define LINUX_COMPILE_HOST "amohseni76-desktop"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
